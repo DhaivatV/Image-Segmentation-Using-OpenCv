3a30d4a380abb27fbf4feacce83d1cb67a928c55
DrDrakeRamoray <71979720+DhaivatV@users.noreply.github.com>
2023-04-25 22:25:00 +0000

    api made

--- test.py
+++
@@ -1,9 +0,0 @@
-import cv2 
-import matplotlib.pyplot as plt 
-image = cv2.imread("meter.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (7, 7), 0)
-thresh = cv2.adaptiveThreshold(blurred, 255, 
-                               cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                              cv2.THRESH_BINARY, 17, 3)
-cv2.imwrite('a1.jpg', thresh)
